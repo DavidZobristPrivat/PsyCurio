afdd7a67fe9409b144eb9d0f92e06ec609729f08
davidzobrist <david.zobrist@smeet.com>
2020-07-18 14:22:45 +0200

    Counter and Items
    Items class and Counter class

---
+++ Assets/Scripts/Generic/Item.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+namespace PsyCurio
+{
+    public class Item : MonoBehaviour, IMouseOver
+    {
+        #pragma warning disable CS0649,CS0108 // Disable warnings that appear with SerializeField
+        
+        [SerializeField] private Renderer renderer;
+        [SerializeField] private Material highlightMaterial;
+     
+        
+        private Material defaultMaterial;
+        private Counter _counter;
+        
+        public bool isOnCounter;
+        public GameObject prefab;
+        
+        private void Awake()
+        {
+            defaultMaterial = renderer.material;
+            _counter = FindObjectOfType<Counter>();
+        }
+
+        public void OnSelect()
+        {
+            // No null check better to get / see the reference error
+            renderer.material = this.highlightMaterial;
+     
+        }
+
+
+        public void OnDeselect()
+        {
+            renderer.material = this.defaultMaterial;
+        
+        }
+
+        public void OnClick()
+        {
+            Debug.Log("Clicked");
+            
+            if (_counter == null)
+            {
+                Debug.LogWarning("Be sure to add a Counter to the world first");
+                return;
+            }
+
+            if (!isOnCounter)
+            {
+               OnDeselect();
+                _counter.TryAddItem(this);
+            }
+            else
+            {
+          
+                Destroy(gameObject);
+            }
+          
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Shop/Cashier/Counter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace PsyCurio
+{
+    public class Counter : MonoBehaviour
+    { 
+        #pragma warning disable CS0649 
+        [SerializeField] private int maximumItems = 5;
+        [SerializeField] private float distanceBetweenObject = 1.4f;  
+        [SerializeField] private Transform spawnedItems;
+      
+      public void TryAddItem(Item _itemToAdd)
+      {
+          if (spawnedItems.childCount < maximumItems)
+          {
+              
+              GameObject tempGameObject = Instantiate(_itemToAdd.prefab,spawnedItems);
+             
+              
+              Item tempItem = tempGameObject.GetComponent<Item>();
+              tempItem.isOnCounter = true;
+          
+              
+              tempGameObject.transform.localPosition = new Vector3(0,0, (distanceBetweenObject*spawnedItems.childCount));
+              tempGameObject.transform.localScale = Vector3.one;
+             
+          }
+      }
+
+  
+
+      public void GetPriceSum()
+      {
+          
+      }
+      
+    }
+
+    
+}
